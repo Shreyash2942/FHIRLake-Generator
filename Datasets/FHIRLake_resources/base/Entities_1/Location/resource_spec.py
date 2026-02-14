80d987b52c9b3058f9d1439c5458f03041c60304
Shreyash <ptlsh4292@gmail.com>
2026-02-13 23:11:32 -0500

    -adding resource_spec for location

---
+++ Datasets/FHIRLake_resources/base/Entities_1/Location/resource_spec.py
@@ -0,0 +1,10 @@
+from core.registry.models import ResourceSpec
+
+RESOURCE_SPEC = ResourceSpec(
+    resource_type="Location",
+    bucket="locations",
+    generator="Datasets.FHIRLake_resources.base.Entities_1.Location.generate_fhit_location:generate",
+    dependencies=["Organization"],
+    required_inputs=[],
+    description="Synthetic Location resource generator.",
+)
